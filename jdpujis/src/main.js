f02e35a66b26aba954419d21f2e901ed33a662c7
Mavlonberdiyev Xudoyberdi <129485330+urgut200404@users.noreply.github.com>
2024-04-05 12:13:33 +0300

    Update main.js
    aos_ochirildi

--- jdpujis/src/main.js
+++ jdpujis/src/main.js
@@ -3,12 +3,10 @@ import './style.css'
 import App from './App.vue'
 import router from './Router/index.js'
 import naive from "naive-ui"
-import aos from "aos"
 const app = createApp(App)
 
 app.use(router)
 app.use(naive)
-app.use(aos)
 app.mount('#app')
 
 
