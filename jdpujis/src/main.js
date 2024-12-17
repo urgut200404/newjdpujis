a5f19acc41d41bdb6dc1106c5f31e24f666ffd80
Mavlonberdiyev Xudoyberdi <129485330+urgut200404@users.noreply.github.com>
2024-12-18 00:52:13 +0300

    Update main.js

--- jdpujis/src/main.js
+++ jdpujis/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './Router/index.js'
-import naive from "naive-ui"
+import router from './Router/index.js' 
+import { create, NButton } from 'naive-ui';
+const naive = create({
+  components: [NButton],
+});
 const app = createApp(App)
-
 app.use(router)
 app.use(naive)
 app.mount('#app')
