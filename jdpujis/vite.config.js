88759bcf4538695ea32ddcab08f4a1da081a6ac3
Mavlonberdiyev Xudoyberdi <129485330+urgut200404@users.noreply.github.com>
2024-12-18 00:50:27 +0300

    Update vite.config.js

--- jdpujis/vite.config.js
+++ jdpujis/vite.config.js
@@ -4,12 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build:{
-    rollupOptions:{
-      output:{
-        manualChunks(id){
-          if(id.includes("node_modules")){
-            return id.toString().split('node_modules')[1].split('/').toString()
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules')[1].split('/')[1];
           }
         }
       }
